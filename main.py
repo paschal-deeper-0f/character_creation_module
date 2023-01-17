ee7990afa70c305012c877cfc1f74f897c09dbb7
paschal-deeper-0f <paschal-deeper-0f@icloud.com>
2023-01-17 16:17:36 +0100

    Add docstring to functions

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """
+    Attack ability usage.
+    :param char_name: character name
+    :param char_class: character class
+    :return: result of usage attack ability
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +23,12 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """
+    Defence ability usage.
+    :param char_name: character name
+    :param char_class: character class
+    :return: result of usage defence ability
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +39,12 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """
+    Usage of special ability.
+    :param char_name: character name
+    :param char_class: character class
+    :return: result of usage special ability
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -38,6 +58,12 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """
+    Training process.
+    :param char_name: character name
+    :param char_class: character class
+    :return: ending training message
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +88,10 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """
+    Character class selection.
+    :return: character class
+    """
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -83,7 +113,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -93,5 +124,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-main()
